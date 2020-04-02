b4602db46df17e342c47b642b34753b1110e7899
Tommaso Cammelli <tomm.cammelli@outlook.it>
2020-04-02 19:30:32 +0200

    aggiunti commenti

--- lezione12/esercizio1.asm
+++ lezione12/esercizio1.asm
@@ -6,21 +6,22 @@ charfine:   .asciiz ""
 
     .text
     .globl main
-main:   la $t0, stringa # Load address, SERVE PER OTTENERE L'INDIRIZZO DI MEMORIA E NON IL CONTENUTO!!! da non confondere con lw
+main:   
+        la $t0, stringa # Load address, SERVE PER OTTENERE L'INDIRIZZO DI MEMORIA E NON IL CONTENUTO!!! da non confondere con lw
         add $t2, $zero, $zero
         la $s5, charfine
         lb $s1, 0($s5) # Load byte - legge un carattere su un byte
 
-ciclo:  
-        lb $s7, 0($t0)
-        beq $s7, $s1, fine
-        addi $t2, $t2, 1
-        addi $t0, $t0, 1
+ciclo:  # Inizio ciclo while
+        lb $s7, 0($t0) # Load byte dell'indirizzo memoria su $t0
+        beq $s7, $s1, fine # Branch on equal, salta su etichetta fine quando i caratteri nei dure registri sono uguali
+        addi $t2, $t2, 1 # Aggiunge 1 al contatore dei caratteri
+        addi $t0, $t0, 1 # Aggiunge 1 all'indirizzo di selezione del carattere 
 
         j ciclo
 
-fine:   
-        la $t3, dim
+fine:   # Parte finale
+        la $t3, dim 
         sw $t2, 0($t3)
 
         li $v0, 4
